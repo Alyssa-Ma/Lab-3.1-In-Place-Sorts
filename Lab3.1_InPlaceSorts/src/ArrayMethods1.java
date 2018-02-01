c910181739e9326e6239cb225eb26ebe1d389d95
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-01 09:23:04 -0500

    bubblesortres

--- Lab3.1_InPlaceSorts/src/ArrayMethods1.java
+++ Lab3.1_InPlaceSorts/src/ArrayMethods1.java
@@ -1,4 +1,8 @@
-//Alyssa Ma 1/17/18
+import java.util.Arrays;
+
+//Lab 3.1 In Place Sorts
+//Author: Alyssa Ma
+//Start Date: 1/17/18
 
 public class ArrayMethods1 {
 	
@@ -10,22 +14,28 @@ public class ArrayMethods1 {
 		String[]test3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
 		
 		//Insertion Sort
-		long start
+		long start = System.nanoTime();
+		insertionSort(test1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Insertion sort time: " + time + "nanoseconds.");
+		System.out.println(Arrays.toString(test1));
 		
 		//Selection Sort
-		
+		start = System.nanoTime();
+		selectionSort(test2);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Selection sort time: " +time + "nanoseconds.");
+		System.out.println(Arrays.toString(test2));
 		
 		//Bubble Sort
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		start = System.nanoTime();
+		bubbleSort(test3);
+		end = System.nanoTime();
+		time = end-start;
+		System.out.println("Bubble sort time: " +time+ "nanoseconds.");
+		System.out.println(Arrays.toString(test3));
 	}
 
 	
@@ -41,8 +51,28 @@ public class ArrayMethods1 {
 	
 	public static void bubbleSort(String [] list1)
 	{
-		int length = list1.length;
-		int x = 0;
+		String x;
+		int swap = 0;
+		
+		while(swap > 0)
+		{
+			for(int i = 0; i<list1.length; i++)
+			{
+				if(list1[i].compareTo(list1[i+1])>0)
+				{
+					x = list1[i];
+					list1[i] = list1[i+1];
+					list1[i+1] = x;
+				}
+			}
+		}
+		for(int d = 0; d<list1.length; d++)
+		{
+			
+		}
+		/**
+		 * int length = list1.length;
+		 * int x = 0;
 		boolean swapAvail = true;
 		while(swapAvail)
 			//while swap >0 needs to keep track of swaps
@@ -61,6 +91,8 @@ public class ArrayMethods1 {
 				}
 			}
 		}
+		 */
+		
 	}
 	
 	
